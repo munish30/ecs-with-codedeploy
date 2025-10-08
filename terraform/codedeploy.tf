a5bbc83e096a045194a6e9d81320f5b28b986d37
Munish <munishrukhaya@gmail.com>
2025-10-08 13:41:17 +0530

    Added codedeploy deployment

---
+++ terraform/codedeploy.tf
@@ -0,0 +1,88 @@
+# ------------------------------------------------------------------------------
+# IAM Role for CodeDeploy
+# ------------------------------------------------------------------------------
+resource "aws_iam_role" "codedeploy_role" {
+  name = "${local.name}-codedeploy-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "codedeploy.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_role_attach" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
+}
+
+# ------------------------------------------------------------------------------
+# CodeDeploy Application
+# ------------------------------------------------------------------------------
+resource "aws_codedeploy_app" "ecs" {
+  name             = "flask-codedeploy-app"
+  compute_platform = "ECS"
+}
+
+# ------------------------------------------------------------------------------
+# CodeDeploy Deployment Group (Blue/Green)
+# ------------------------------------------------------------------------------
+resource "aws_codedeploy_deployment_group" "ecs" {
+  app_name               = aws_codedeploy_app.ecs.name
+  deployment_group_name  = "flask-deployment-group"
+  service_role_arn       = aws_iam_role.codedeploy_role.arn
+  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
+
+  ecs_service {
+    cluster_name = "ecs-assignment" # Replace with your ECS cluster
+    service_name = "ecs-assignment"  # Replace with your ECS service name
+  }
+
+  deployment_style {
+    deployment_type   = "BLUE_GREEN"
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+  }
+
+
+  blue_green_deployment_config {
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+    }
+
+    green_fleet_provisioning_option {
+      action = "COPY_AUTO_SCALING_GROUP"
+    }
+  }
+
+  load_balancer_info {
+    target_group_pair_info {
+      prod_traffic_route {
+        listener_arns = [module.alb.listeners["ex_http"].arn]
+      }
+
+      test_traffic_route {
+        listener_arns = [module.alb.listeners["ex_http"].arn]
+      }
+
+      target_group {
+        name = module.alb.target_groups["ex_ecs"].name
+      }
+
+      target_group {
+        name = module.alb.target_groups["ex_ecs"].name
+      }
+    }
+  }
+}
